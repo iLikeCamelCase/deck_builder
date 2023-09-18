14e62f009aed5efabf97793d25be90aa0560e1d1
Julian <diceccojulian@gmail.com>
2023-09-18 11:40:33 -0400

    update main
    program works and has basic functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import file_reading as fr
-import translation as tr
+import translation2 as tr
 import deck_builder as db
 
 def create_deck_from_file(file: str, filetype: str, source: str, dest: str,
@@ -7,7 +7,7 @@ def create_deck_from_file(file: str, filetype: str, source: str, dest: str,
     
     match filetype:
         case 'pdf':
-            reader = fr.ReadPDF('/uploads/'+file)
+            reader = fr.ReadPDF('uploads/'+file)
         case 'docx':
             #TODO docx
             raise ValueError(f"Unsupported file type: {filetype}")
@@ -21,10 +21,15 @@ def create_deck_from_file(file: str, filetype: str, source: str, dest: str,
             raise ValueError(f"Unsupported file type: {filetype}")
         
     ordered_word_list = reader.ordered
+    print(type(ordered_word_list))
     translated_tuple_list = tr.translate_list_words(ordered_word_list, source, dest)
+    #print(translated_tuple_list)
+    print('Translated!')
     # create anki deck file from tuple list
     db.word_list_to_file(translated_tuple_list, path, deckname)
+    print('Done Deck!')
 
 
 if __name__ == '__main__':
-    pass
+    create_deck_from_file('test.pdf','pdf','fr','en',
+                          'Plan de cours','created/')
